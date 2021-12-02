58b9f88f14ae89a7056cf7390480ec14b5b60acf
bogdanchumak <56061966+bogdanchumak@users.noreply.github.com>
2021-12-02 13:08:16 +0200

    Update task_2.py

--- HT_5/task_2.py
+++ HT_5/task_2.py
@@ -2,7 +2,7 @@
 Створіть функцію для валідації пари ім'я/пароль за наступними правилами:
    - ім'я повинно бути не меншим за 3 символа і не більшим за 50;
    - пароль повинен бути не меншим за 8 символів і повинен мати хоча б одну цифру;
-   - щось своє :)
+   - щось своє :) (пароль повинен містити хоча б одну велику букву)
    Якщо якийсь із параментів не відповідає вимогам - породити виключення із відповідним текстом.
 '''
 
@@ -13,16 +13,23 @@ class ValidationError(Exception):
 
 def validation(username, password):
     number = 0
+    upper = 0
+    for j in password:
+        if j.isupper():
+            upper += 1
     for i in password:
         if i.isdigit():
             number += 1
-    if 3 < len(username) < 50 and (len(password) > 8 and number > 0):
+    if 3 < len(username) < 50 and (len(password) > 8 and number > 0 and upper > 0):
         print('Всі дані вірні')
-    elif len(username) > 50 or len(username) < 3:
+    elif len(username) > 50 or len(username) <= 3:
         raise ValidationError("ім'я повинно бути не меншим за 3 символа і не більшим за 50")
     elif len(password) < 8:
         raise ValidationError('Пароль повинен бути не меншим за 8 символів')
     elif number == 0:
         raise ValidationError('Пароль повинен містити хоча б одну цифру')
+    elif upper == 0:
+        raise ValidationError('пароль повинен містити хоча б одну велику букву') # ЩОСЬ СВОЄ :)
 
-validation('gs', '1fhfghhfgh')
+
+validation('gs33', '1fhfghhfgh')
