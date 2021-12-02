469a4fafccc50d8b47e4478dfd9f3a4b2344c9fc
bogdanchumak <56061966+bogdanchumak@users.noreply.github.com>
2021-12-02 12:30:01 +0200

    Update task_2.py

--- HT_5/task_2.py
+++ HT_5/task_2.py
@@ -18,9 +18,11 @@ def validation(username, password):
             number += 1
     if 3 < len(username) < 50 and (len(password) > 8 and number > 0):
         print('Всі дані вірні')
-    elif len(username) > 50:
-        print('Введіть нормальний логін')
-    else:
-        raise ValidationError('Something went wrong')
+    elif len(username) > 50 or len(username) < 3:
+        raise ValidationError("ім'я повинно бути не меншим за 3 символа і не більшим за 50")
+    elif len(password) < 8:
+        raise ValidationError('Пароль повинен бути не меншим за 8 символів')
+    elif number == 0:
+        raise ValidationError('Пароль повинен містити хоча б одну цифру')
 
-validation('gsdgsd', '1gfhfghfgh')
+validation('gs', '1fhfghhfgh')
