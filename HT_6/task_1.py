d6e45bf55f0fd2d10638edecd5b6d525060f8121
bogdanchumak <56061966+bogdanchumak@users.noreply.github.com>
2021-12-05 12:39:28 +0200

    Add files via upload

---
+++ HT_6/task_1.py
@@ -0,0 +1,33 @@
+'''
+Програма-світлофор.
+   Створити програму-емулятор світлофора для авто і пішоходів.
+   Після запуска програми на екран виводиться в лівій половині - колір автомобільного,
+   а в правій - пішохідного світлофора.
+   Кожну секунду виводиться поточні кольори. Через декілька ітерацій -
+   відбувається зміна кольорів - логіка така сама як і в звичайних світлофорах.
+   Приблизний результат роботи наступний:
+      Red        Green
+      Red        Green
+      Red        Green
+      Red        Green
+      Yellow     Green
+      Yellow     Green
+      Green      Red
+      Green      Red
+      Green      Red
+      Green      Red
+      Yellow     Red
+      Yellow     Red
+      Red        Green
+      .......
+'''
+import time
+
+cars_traffic_light = ['Red', 'Red', 'Red', 'Red', 'Yellow', 'Yellow', 'Green', 'Green', 'Green', 'Green', 'Yellow', 'Yellow']
+
+pedestrian_traffic_light = ['Green', 'Green', 'Green', 'Green', 'Green', 'Green', 'Red', 'Red', 'Red', 'Red', 'Red', 'Red']
+
+while True:
+    for i, j in zip(cars_traffic_light, pedestrian_traffic_light):
+        print(i, '      ', j)
+        time.sleep(1)
